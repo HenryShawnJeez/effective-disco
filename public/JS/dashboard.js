782300c14512d465d83b7cc6f8e9496c99e7d6bd
KingZedPriest <Charleschukwuemeka47@gmail.com>
2023-08-07 11:55:49 +0100

    Fix: The Chart height, Nav Bar and Footer Address

--- public/JS/dashboard.js
+++ public/JS/dashboard.js
@@ -3,7 +3,6 @@ function dropDown() {
   document.querySelector('#submenu').classList.toggle('hidden')
   document.querySelector('#arrow').classList.toggle('rotate-180')
 }
-dropDown()
 
 function Openbar() {
   document.querySelector('.sidebar').classList.toggle('left-[-300px]')
