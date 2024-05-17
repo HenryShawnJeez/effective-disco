9efddbfb799073f48473ce2aa9c9fcd15b6673bd
Priest <Charleschukwuemeka47@gmail.com>
2024-05-17 12:58:45 +0100

    Fix: Made a clients edit to plans

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ const capitalDuration = 1209600000; //14Days
 const advancedDuration = 1814400000; //21Days
 const ultimateDuration = 2505600000; //29Days
 const essentialPercent = 0.21; //21% 3% Daily for 7 Days
-const capitalPercent = 0.84; //84% 6% Daily for 14 Days
-const advancedPercent = 1.68; //168% 8% Daily for 21 Days
-const ultimatePercent = 3.19; //319% 11% daily profit in 29 days
+const capitalPercent = 0.70; //70% 5% Daily for 14 Days
+const advancedPercent = 1.47; //147% 7% Daily for 21 Days
+const ultimatePercent = 2.90; //290% 10% daily profit in 29 days
 const referralEarningPercent = 0.05; //5%
 
 
