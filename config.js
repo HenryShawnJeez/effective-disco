d655534543e9f37a7366242d843fd99b096012e0
HenryShawnJeez <145283087+HenryShawnJeez@users.noreply.github.com>
2024-11-08 18:22:50 +0100

    Update config.js
    Add the new plan configs

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ const PORT = process.env.PORT || 3000;
 const essentialDuration =  604800000 ; //7Days 54000000(3mins For testing)
 const capitalDuration = 1209600000; //14Days
 const advancedDuration = 1814400000; //21Days
-const ultimateDuration = 2505600000; //29Days
+const ultimateDuration = 1209600000; //14Days
 const essentialPercent = 0.21; //21% 3% Daily for 7 Days
-const capitalPercent = 0.70; //70% 5% Daily for 14 Days
-const advancedPercent = 1.47; //147% 7% Daily for 21 Days
-const ultimatePercent = 2.90; //290% 10% daily profit in 29 days
+const capitalPercent = 0.84; //84% 6% Daily for 14 Days
+const advancedPercent = 1.68; //168% 8% Daily for 21 Days
+const ultimatePercent = 1.54; //154% 11% daily profit in 14 days
 const referralEarningPercent = 0.05; //5%
 
 
